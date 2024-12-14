bd4811aed32c21626d4ba88cb3cf0bf041928b3c
Meykils <meykils.kin@gmail.com>
2024-12-14 14:33:56 +0200

    lesson 2 js

---
+++ basic_js/lesson2/index.js
@@ -0,0 +1,22 @@
+var name = "Axen, Alexandr, Misha, Alex, Bogdan"; // string
+
+let text = "This variable has been created with let"; //string
+
+let emptyVariable; // undefined
+
+emptyVariable = name; // "Axen, Alexandr, Misha, Alex, Bogdan"
+emptyVariable = text;
+
+const kebabKing = "I'm kebab king";
+
+let number = -12; //number : 1,2,3,4,5,6,7,8,9,0
+
+const booleanTrue = true; //boolean
+const booleanFalse = false; //boolean
+
+console.log(booleanTrue)
+console.log(booleanFalse)
+console.log(kebabKing)
+console.log(number)
+console.log(emptyVariable)
+console.log(text)
