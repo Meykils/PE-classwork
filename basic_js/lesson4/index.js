2065426311ee58ba21ff5107b34d6655ae5f37a7
Meykils <meykils.kin@gmail.com>
2025-02-15 12:49:49 +0200

    test

---
+++ basic_js/lesson4/index.js
@@ -0,0 +1,27 @@
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+
+let limit = 0;
+let start = 10;
+
+//       0  >   0  true
+while (start > limit) {
+  console.log("Hello world!" + start );
+  start--;
+}
+// 1st iteration
+// iteration start = 0
+// start <= limit  - true
+// 2nd iteration
+//  iteration start = 1
+//  start <= limit - true
+// .....
+//
